67117d9caa3b02b93875fd41f1626c219f9b1397
Arifmun8 <111012843+Arifmun8@users.noreply.github.com>
2022-08-17 21:54:16 +0700

    Delete kemampuan-dasar/kemampuan-dasar-1 directory

--- kemampuan-dasar/kemampuan-dasar-1/cari-java.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-find=`find $1 -name "*.java" -exec dirname {} \; | sort -u`
-
-if [[ `echo $find | grep / | wc -l` -ne 0 ]]; then
-#printf '%s\n' "Ada file Java pada direktori" $find
-printf '%s\n' $find | awk '{print "Ada file Java pada direktori",$0}'
-fi
